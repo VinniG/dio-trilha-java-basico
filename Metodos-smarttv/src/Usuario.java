ef68ecd8a10501d72524412b8d080d378aab4ca6
VinniG <VinniG@noreply.github.com>
2023-09-07 23:36:34 -0300

    método smartTV

---
+++ Metodos-smarttv/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV Ligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual: " + smartTv.volume);
+        
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual: "+ smartTv.canal);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV Ligada? " + smartTv.ligada);
+    }
+
+}
